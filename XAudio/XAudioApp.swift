19441bc21c7cb8bec8fa5aaa9bd0e5b5c26d5ddb
heinzd <heinz.dittmann@gmx.de>
2026-09-10 15:12:58 +0200

    Allow landscape through app delegate

--- XAudio/XAudioApp.swift
+++ XAudio/XAudioApp.swift
@@ -1,7 +1,20 @@
 import SwiftUI
+import UIKit
+
+final class XAudioAppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        .allButUpsideDown
+    }
+}
 
 @main
 struct XAudioApp: App {
+    @UIApplicationDelegateAdaptor(XAudioAppDelegate.self)
+    private var appDelegate
+
     @State private var model = AudioLibraryModel()
 
     var body: some Scene {
@@ -10,4 +23,3 @@ struct XAudioApp: App {
         }
     }
 }
-
