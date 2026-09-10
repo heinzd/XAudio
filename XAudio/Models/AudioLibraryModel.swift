6e13bc3c68c11f819acd68520f06f593232ef672
heinzd <heinz.dittmann@gmx.de>
2026-09-10 20:33:40 +0200

    Preserve playback position when leaving playlist

--- XAudio/Models/AudioLibraryModel.swift
+++ XAudio/Models/AudioLibraryModel.swift
@@ -168,9 +168,7 @@ final class AudioLibraryModel {
 
     func stopPlayback() {
         player.pause()
-        player.replaceCurrentItem(with: nil)
         isPlaying = false
-        elapsed = 0
         updateNowPlayingInfo()
     }
 
